95c0714aeb9c20916f7a4226f918db9b9a8a4671
Aikaterini Karakoulaki (HF PhD 2024) <bm24156@hm01.dice.priv>
2025-05-09 13:02:37 +0000

    changed model

--- tagger/train/models.py
+++ tagger/train/models.py
@@ -9,7 +9,8 @@ from HGQ.layers import HDense, HConv1D, PAveragePooling1D,  HQuantize, HActivati
 from qkeras.quantizers import quantized_bits, quantized_relu
 from qkeras.qlayers import QDense, QActivation
 from qkeras import QConv1D
-
+from HGQ.utils import get_default_paq_conf, get_default_kq_conf
+from HGQ.utils import set_default_paq_conf, set_default_kq_conf
 
 def baseline(inputs_shape, output_shape, bits=9, bits_int=2, alpha_val=1):
 
@@ -68,40 +69,36 @@ def baseline(inputs_shape, output_shape, bits=9, bits_int=2, alpha_val=1):
 
 
 def deepset_HGQ(inputs_shape, output_shape):
-    
+    paq_conf = get_default_paq_conf()
+    paq_conf['skip_dims'] = 'except_last' # or just 'all'
+    set_default_paq_conf(paq_conf)
+        
+    L, C = inputs_shape
     inputs = tf.keras.layers.Input(shape=inputs_shape, name='model_input')
-  
+
     #Main branch
-    main = HQuantize(name='norm_input',beta=3e-5)(inputs)
-    main = HConv1DBatchNorm(filters=10, kernel_size=1, beta=1.1e-5,parallel_factor=1,activation='relu',name='Conv1D_1')(main)
+    main = HQuantize(name='quant1' ,beta=1.1e-5)(inputs) 
     
-    main = HConv1D(filters=10, kernel_size=1,beta=1.1e-5,parallel_factor=1,activation='relu',name='Conv1D_2')(main)
-
-    sequence_length = main.shape[1]
-    main = PAveragePooling1D(pool_size=sequence_length, name="avgpool")(main)
+    
+    main = HConv1DBatchNorm(filters=10, activation='relu',kernel_size=1, beta=1.1e-5, parallel_factor=1, name='Conv1D_1')(main)
+    main = HConv1D(filters=10, activation='relu', kernel_size=1, beta=1.1e-5, parallel_factor=1, name='Conv1D_2')(main)
+   
+    main = PAveragePooling1D(L, name='avgpool')(main)
     main = PFlatten()(main)
-    
     #jetID branch, 3 layer MLP
-    jet_id = HDense(32, beta=1.1e-5,parallel_factor=1,name='Dense_1_jetID')(main)
-    jet_id = HActivation(activation='relu', beta=1.1e-5,name='relu_1_jetID')(jet_id)
-
-    jet_id = HDense(16, name='Dense_2_jetID',beta=1.1e-5,parallel_factor=1)(jet_id)
-    jet_id = HActivation(activation='relu',beta=1.1e-5, name='relu_2_jetID')(jet_id)
-
     
+    jet_id = HDense(32, beta=1.1e-5, activation='relu',parallel_factor=1,name='Dense_1_jetID')(main)
+    jet_id = HDense(16, name='Dense_2_jetID',beta=1.1e-5, activation='relu',parallel_factor=1)(jet_id)
+     
     jet_id = HDense(output_shape[0],beta=1.1e-5, name='Dense_3_jetID')(jet_id)
     jet_id = Activation('softmax', name='act_jet')(jet_id)
     jet_id = Signature(bits=18, int_bits=8, keep_negative=0, name='jet_id_output')(jet_id)
-
-
     #pT regression branch
-    pt_regress = HDense(10, name='Dense_1_pT', parallel_factor=1,beta=1.1e-5)(main)
-    pt_regress = HActivation(activation='relu',beta=1.1e-5, name='relu_1_pt')(pt_regress)
+    pt_regress = HDense(10, name='Dense_1_pT', parallel_factor=1,beta=1.1e-5, activation='relu')(main)
 
     
     pt_regress = HDense(1, beta=1.1e-5,parallel_factor=1,name='pT_out')(pt_regress)
     pt_regress = Signature(bits=16, int_bits=6, keep_negative=0, name='pT_output')(pt_regress)
     #Define the model using both branches
     model = tf.keras.Model(inputs = inputs, outputs = [jet_id, pt_regress])
-
     return model

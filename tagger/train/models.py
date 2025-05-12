a041305f1d8d5d4e8b5880542693338c17a10097
Chriisbrown <chris.brown@fpsl.net>
2025-05-12 11:27:07 +0200

    multitrehad test

--- tagger/train/models.py
+++ tagger/train/models.py
@@ -2,8 +2,7 @@
 Here all the models are defined to be called in train.py
 """
 import keras
-from keras.layers import BatchNormalization, Input, Activation, GlobalAveragePooling1D, Dense, Conv1D
-
+from keras.layers import BatchNormalization, Input, Activation, GlobalAveragePooling1D,AveragePooling1D, Dense, Conv1D, Flatten
 #HGQ
 from hgq.layers import QDense, QConv1D ,QBatchNormalization
 from hgq.layers.softmax import QSoftmax
@@ -13,6 +12,7 @@ from hgq.regularizers import MonoL1
 
 
 def baseline(inputs_shape, output_shape):
+    L, C = inputs_shape
     #Initialize inputs
     inputs = keras.layers.Input(shape=inputs_shape, name='model_input')
     
@@ -28,8 +28,9 @@ def baseline(inputs_shape, output_shape):
     #main = QActivation(activation=quantized_relu(bits), name='relu_2')(main)
 
     # Linear activation to change HLS bitwidth to fix overflow in AveragePooling
-    main = Activation(activation='linear', name = 'act_pool')(main)
-    main = GlobalAveragePooling1D(name='avgpool')(main)
+    #main = Activation(activation='linear', name = 'act_pool')(main)
+    main = AveragePooling1D(L,name='avgpool')(main)
+    main = Flatten()(main)
 
     #Now split into jet ID and pt regression
 
@@ -56,3 +57,43 @@ def baseline(inputs_shape, output_shape):
     print(model.summary())
 
     return model
+
+
+# from HGQ.utils import get_default_paq_conf, get_default_kq_conf
+# from HGQ.utils import set_default_paq_conf, set_default_kq_conf
+
+
+# def deepset_HGQ(inputs_shape, output_shape):
+#     paq_conf = get_default_paq_conf()
+#     paq_conf['skip_dims'] = 'except_last' # or just 'all'
+#     set_default_paq_conf(paq_conf)
+        
+#     L, C = inputs_shape
+#     inputs = keras.layers.Input(shape=inputs_shape, name='model_input')
+
+#     #Main branch
+#     main = BatchNormalization(name='norm')(inputs)
+#     main = HQuantize(name='quant1' ,beta=3e-5)(main) 
+#     main = HConv1D(filters=10, activation='relu',kernel_size=1, beta=1.1e-5, parallel_factor=1, name='Conv1D_1')(main)
+#     main = HConv1D(filters=10, activation='relu', kernel_size=1, beta=1.1e-5, parallel_factor=1, name='Conv1D_2')(main)
+   
+#     main = PAveragePooling1D(L, name='avgpool')(main)
+
+#     #jetID branch, 3 layer MLP
+    
+#     jet_id = HDense(32, beta=1.1e-5, activation='relu',parallel_factor=1,name='Dense_1_jetID')(main)
+#     jet_id = HDense(16, name='Dense_2_jetID',beta=1.1e-5, activation='relu',parallel_factor=1)(jet_id)
+     
+#     jet_id = HDense(output_shape[0],beta=1.1e-5, name='Dense_3_jetID', activation='relu')(jet_id)
+
+#     #pT regression branch
+#     pt_regress = HDense(10, name='Dense_1_pT', parallel_factor=1,beta=1.1e-5, activation='relu')(main)
+
+    
+#     pt_regress = HDense(1, beta=1.1e-5,parallel_factor=1,name='pT_out')(pt_regress)
+#     #Define the model using both branches
+#     model = keras.Model(inputs = inputs, outputs = [jet_id, pt_regress])
+#     return model
+
+# loss_classification = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# loss_regression = keras.losses.MeanSquaredError()
---
+++ tagger/train/multithread_test.py
@@ -0,0 +1,52 @@
+from joblib import Parallel, delayed, parallel_backend
+import os
+from math import sqrt
+import time
+
+def timeit(f):  
+    """Measures execution time."""  
+    def wrap(*args, **kwargs):  
+        t1 = time.time()  
+        res = f(*args, **kwargs)  
+        print(f"{f.__name__} ran in {time.time() - t1:.6f}s")  
+        #return res  
+    return wrap  
+
+os.system('nproc')
+
+@timeit
+def parallel_in_parallel_wrapper(n=10,n_threads=2,backend='loky'):
+
+    
+    if backend!='loky':
+        with parallel_backend(backend):
+            intermediate_out = Parallel(n_jobs=n_threads)(delayed(sqrt)(i ** 2) for i in range(n))
+
+    else:
+        with parallel_backend(backend,inner_max_num_threads=2):
+             intermediate_out = Parallel(n_jobs=n_threads)(delayed(sqrt)(i ** 2) for i in range(n))
+    #return intermediate_out
+
+
+threads = [1,2,4,8,12,24]
+n_iterations = [10000,1000000,10000000]
+
+for thread in threads:
+    for iterations in n_iterations:
+        print("=========================")
+        print("n threads: ",thread)
+        print("n iterations: ",iterations)
+        print("+++++++++++++")
+        print("single thread 2 backend thread loky: ")
+        parallel_in_parallel_wrapper(iterations,n_threads=thread,backend='loky')
+        print("+++++++++++++")
+        print("multi thread 2 backend thread loky: ")
+        parallel_in_parallel_wrapper(iterations,n_threads=thread,backend='loky')
+        print("+++++++++++++")
+        print("multi thread threading: ")
+        parallel_in_parallel_wrapper(iterations,n_threads=thread,backend='threading')
+        print("+++++++++++++")
+        print("single thread sequential: ")
+        parallel_in_parallel_wrapper(iterations,n_threads=thread,backend='sequential')
+
+        print("=========================")

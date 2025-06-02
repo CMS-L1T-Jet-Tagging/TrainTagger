e768caf89407545e4b067375e3299614c78f1769
Stella <stella.felice.schaefer@desy.de>
2025-06-02 12:31:58 +0200

    label cosmetics for dp note

--- tagger/plot/bbtt.py
+++ tagger/plot/bbtt.py
@@ -531,7 +531,7 @@ def bbtt_eff_HT(model_dir, signal_path, score_type, apply_sel, n_entries=100000,
     ax.set_ylim([0., 1.15])
     ax.set_xlim([0, 800])
     ax.set_xlabel(r"$HT^{gen}$ [GeV]")
-    ax.set_ylabel(r"$\epsilon$(HH $\to$ bb$\tau \tau$ trigger rate at {} kHz)".format(rate))
+    ax.set_ylabel(r"$\epsilon$(HH $\to$ bb$\tau \tau$)")
     plt.legend(loc='upper left')
 
     #Save plot
@@ -555,7 +555,7 @@ def bbtt_eff_HT(model_dir, signal_path, score_type, apply_sel, n_entries=100000,
     ax2.set_ylim([0., 1.15])
     ax2.set_xlim([0, 800])
     ax2.set_xlabel(r"$HT^{gen}$ [GeV]")
-    ax2.set_ylabel(r"$\epsilon$(HH $\to$ bb$\tau \tau$ trigger rate at 14 kHz")
+    ax2.set_ylabel(r"$\epsilon$(HH $\to$ bb$\tau \tau$)")
     plt.legend(loc='upper left')
 
     #Save plot
@@ -624,7 +624,7 @@ def bbtt_eff_mHH(model_dir,
     fig, ax = plt.subplots(1, 1, figsize=style.FIGURE_SIZE)
     hep.cms.label(llabel=style.CMSHEADER_LEFT, rlabel=style.CMSHEADER_RIGHT, ax=ax, fontsize=style.MEDIUM_SIZE-2)
 
-    hep.histplot((normalized_counts, bin_edges), ax=ax, histtype='step', color='grey', label=r"$mHH^{gen}$")
+    hep.histplot((normalized_counts, bin_edges), ax=ax, histtype='step', color='grey', label=r"$m_{HH}^{gen}$")
     ax.errorbar(model_x, model_y, yerr=model_err, c=style.color_cycle[1], fmt='o', linewidth=3,
                 label=model_label)
     ax.errorbar(ht_only_x, ht_only_y, yerr=ht_only_err, c=style.color_cycle[2], fmt='o', linewidth=3,

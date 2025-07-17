e4e2d53213d5bf6fff2f3c4f9583c519d20caa2e
arcox <a.cox24@imperial.ac.uk>
2025-07-17 21:20:52 +0100

    Added transformer model

--- tagger/model/__init__.py
+++ tagger/model/__init__.py
@@ -2,3 +2,4 @@
 from tagger.model.DeepSetModel import DeepSetModel
 from tagger.model.DeepSetModelHGQ import DeepSetModelHGQ
 from tagger.model.InteractionNetModel import InteractionNetModel
+from tagger.model.TransformerModel import TransformerModel

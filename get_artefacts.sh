18a074cebb282361322d811582cb8b507055071a
Christopher Brown <chris.brown@fpsl.net>
2025-08-09 18:47:43 +0100

    Update get_artefacts.sh

--- get_artefacts.sh
+++ get_artefacts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for d in $1 ; do
-    cp $1/$d/latest/plots/training/basic_ROC.png basic_ROC_$d.png
+for dir in $1/* ; do
+    cp $1/$dir/latest/plots/training/basic_ROC.png basic_ROC_$d.png
 done
 

6205853116508495c34b9b99fb4c784ae0e588d8
Chriisbrown <chris.brown@fpsl.net>
2025-08-11 16:03:34 +0200

    get artefacts

--- CI/get_artefacts.sh
+++ CI/get_artefacts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir outputs
-for d in $1 ; do
-    echo $d 
-    echo $1/$d
-    cp $1/$d/latest/plots/plotting_dict.pkl outputs/${d}_plotting_dict.pkl
-done
+# for d in $1 ; do
+#     echo $d 
+#     echo $1/$d
+#     cp $d/latest/plots/plotting_dict.pkl outputs/${d}_plotting_dict.pkl
+# done
 
-python tagger/plot/merge_ROCs.py outputs
+python tagger/plot/merge_ROCs.py $1
 

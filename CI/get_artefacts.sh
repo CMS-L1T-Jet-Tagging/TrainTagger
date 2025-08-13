29fb43735e4003796d94114a70f626e348478b90
Chriisbrown <chris.brown@fpsl.net>
2025-08-13 13:46:22 +0200

    fix bash script

--- CI/get_artefacts.sh
+++ CI/get_artefacts.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 mkdir outputs
-eos cp -r ${1}* outputs
+
+array=($(eos ls ${1}))
+
+for i in "${array[@]}"
+do
+   echo "$i"
+   echo "======="
+   mkdir outputs/$i
+   eos cp -r ${1}/$i/latest/plots/*.pkl outputs/$i
+
+done
 
 python tagger/plot/merge_ROCs.py outputs
 

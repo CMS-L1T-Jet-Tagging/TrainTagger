b8f5f1bbfe18a29905677eea2752bc210d33a179
Christopher Brown <chris.brown@fpsl.net>
2025-08-14 20:15:17 +0200

    Use 15_1_pre1_L1TSC4NGJetTagger-dev fastpuppi

--- CI/setup_cmssw.sh
+++ CI/setup_cmssw.sh
@@ -45,7 +45,7 @@ make
 make install
 cd ..
 
-git clone https://github.com/CMS-L1T-Jet-Tagging/FastPUPPI.git -b 15_1_0/L1TSC4NGJetTagger
+git clone https://github.com/CMS-L1T-Jet-Tagging/FastPUPPI.git -b 15_1_pre1_L1TSC4NGJetTagger-dev
 
 
 if [[ "$COMPILE" == "false" ]]; then exit 0; fi

13a6892619c01db10c686d85a49d3e6641d1a603
Chriisbrown <chris.brown@fpsl.net>
2025-10-16 12:58:42 +0200

    Even more model agnostic in basic

--- tagger/plot/basic.py
+++ tagger/plot/basic.py
@@ -854,7 +854,7 @@ def basic(model, signal_dirs):
     truth_pt_test = np.load(f"{model.output_directory}/testing_data/truth_pt_test.npy")
     reco_pt_test = np.load(f"{model.output_directory}/testing_data/reco_pt_test.npy")
 
-    model_outputs = model.jet_model.predict(X_test)
+    model_outputs = model.predict(X_test)
 
     # Get classification outputs
     y_pred = model_outputs[0]
@@ -880,7 +880,7 @@ def basic(model, signal_dirs):
             signal_indices, sample_train, sample_test = filter_process(X_test, signal_dirs[i])
             sample_data = np.concatenate((sample_train[0], sample_test[0]), axis=0)
             sample_labels = np.concatenate((sample_train[1], sample_test[1]), axis=0)
-            sample_preds = model.jet_model.predict(sample_data)[0]
+            sample_preds = model.predict(sample_data)[0]
             y_p, y_t = y_pred[signal_indices], y_test[signal_indices]
             process_label = process_labels(signal_dirs[i])
             os.makedirs(binary_dir, exist_ok=True)

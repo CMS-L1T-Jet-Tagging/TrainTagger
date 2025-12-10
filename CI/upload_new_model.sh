37447851c982088f71eaced16530069d42a2f18c
Christopher Brown <chris.brown@fpsl.net>
2025-12-10 08:53:34 +0100

    Package up the fw properly

--- CI/upload_new_model.sh
+++ CI/upload_new_model.sh
@@ -18,7 +18,7 @@ mv output/$Model/plots/training/ $Name/plots
 mv output/$Model/plots/physics/ $Name/plots
 
 if [[ "$RUN_SYNTHESIS" == "True" ]]; then
-    tar -cvf L1TSC4NGJetModel.tgz tagger/firmware
+    tar -cvf L1TSC4NGJetModel.tgz output/$Model/firmware/L1TSC4NGJetModel
     eos mkdir -p ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/firmware/
     cp -r L1TSC4NGJetModel.tgz ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/firmware/
     mv output/$Model/plots/profile $Name/plots

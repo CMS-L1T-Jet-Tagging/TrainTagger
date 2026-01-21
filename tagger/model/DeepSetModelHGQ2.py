7335dd47a0901581798e1725e02a0b6f89d6c020
Chriisbrown <chris.brown@fpsl.net>
2026-01-21 09:50:57 +0100

    Use raw hsl4ml no da4ml

--- tagger/model/DeepSetModelHGQ2.py
+++ tagger/model/DeepSetModelHGQ2.py
@@ -153,7 +153,7 @@ class DeepSetModelHGQ2(JetTagModel):
 
             # Create default config
             config = hls4ml.utils.config_from_keras_model(self.jet_model, granularity='name')
-            config["Model"]["Strategy"]="distributed_arithmetic"
+            #ßconfig["Model"]["Strategy"]="distributed_arithmetic"
             config["Model"]["ReuseFactor"]=1
             config['IOType'] = 'io_parallel'
            
--- tagger/model/JEDILinearHGQ2.py
+++ tagger/model/JEDILinearHGQ2.py
@@ -160,7 +160,7 @@ class JEDILinearHGQ2(JetTagModel):
 
             # Create default config
             config = hls4ml.utils.config_from_keras_model(self.jet_model, granularity='name')
-            config["Model"]["Strategy"]="distributed_arithmetic"
+            #config["Model"]["Strategy"]="distributed_arithmetic"
             config["Model"]["ReuseFactor"]=1
             config['IOType'] = 'io_parallel'
             

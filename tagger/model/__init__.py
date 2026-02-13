a8cefe70671034c6b2a00753015a6bde1ec05db5
Stella <stella.felice.schaefer@desy.de>
2026-02-13 09:15:39 +0100

    remove model class

--- tagger/model/__init__.py
+++ tagger/model/__init__.py
@@ -13,4 +13,3 @@ from tagger.model.DeepSetFeaturesModel import DeepSetFeaturesModel
 from tagger.model.DeepSetModelHGQ import DeepSetModelHGQ
 from tagger.model.InteractionNetModel import InteractionNetModel
 from tagger.model.QKerasModel import QKerasModel
-from tagger.model.LinearFitModel import LinearFitModel

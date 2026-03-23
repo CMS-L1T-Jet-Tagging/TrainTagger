a7264cc7a854966141438675c2b0a38030a97110
Stella <stella.felice.schaefer@desy.de>
2026-03-23 13:19:47 +0100

    setup for WeighPU3

--- tagger/model/QKerasModel.py
+++ tagger/model/QKerasModel.py
@@ -112,7 +112,7 @@ class QKerasModel(JetTagModel):
         if 'initial_sparsity' in self.training_config:
             self._prune_model(num_samples)
 
-        def asymmetric_huber_loss(delta=.1, pu=1.5, alpha=1.5):
+        def asymmetric_huber_loss(delta=.1, pu=1.5, alpha=2.):
             """
             Huber loss with asymmetric penalization.
 

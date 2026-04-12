29d3d597a8d68d06dd348442e24d2fb8e297ccae
Stella <stella.felice.schaefer@desy.de>
2026-04-12 13:49:51 +0200

    setup for Pt2PU2, use just raw pt as weight, alpha=3, pu=0

--- tagger/model/QKerasModel.py
+++ tagger/model/QKerasModel.py
@@ -112,7 +112,7 @@ class QKerasModel(JetTagModel):
         if 'initial_sparsity' in self.training_config:
             self._prune_model(num_samples)
 
-        def huber_loss(delta=.1, pu=0., alpha=0.):
+        def huber_loss(delta=.1, pu=1., alpha=3.):
             """
             Huber loss with asymmetric penalization.
 

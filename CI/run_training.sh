c2d020ad1107348244738cc57a8e77aff95a9b80
Christopher Brown <chris.brown@fpsl.net>
2026-04-30 09:55:36 +0200

    Add option to run on specific model pipeline ID

--- CI/run_training.sh
+++ CI/run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $1 RERUN_ON_TAG; if false triggers full retraining of the model, if true just run basic plotting on the tagged model
-
-if [[ "$1" == "False" ]]; then
+# $2 RERUN_ON_OLDTRAINING if true run basic plotting on previosuly trained model
+if [ "$1" == "False" ] && [ "$2" == "False" ]; then
     python tagger/train/train.py -p 75 -y tagger/model/configs/$Model.yaml -o output/$Model
     eos cp ${EOS_STORAGE_DIR}/${EOS_STORAGE_DATADIR}/signal_process_data.tgz .
     tar -xf signal_process_data.tgz
@@ -9,11 +9,26 @@ if [[ "$1" == "False" ]]; then
     cd output/$Model/model
     eos mkdir -p ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/model
     eos cp saved_model.keras ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/model/
+    cd ..
+    eos cp *.json ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/
+    eos cp *.yaml ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/
+    eos cp -r testing_data ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/
     export MODEL_LOCATION=${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}
-else
+
+elif [ "$1" == "False" ] && [ "$2" == "True" ]; then
     mkdir -p output/$Model/model
     eos cp ${MODEL_LOCATION}/model/saved_model.* output/$Model/model
-    eos cp ${MODEL_LOCATION}/extras/* output/$Model/
+    eos cp ${MODEL_LOCATION}/*.json output/$Model/
+    eos cp ${MODEL_LOCATION}/*.yaml output/$Model/
+    mkdir -p output/$Model/testing_data
+    eos cp ${MODEL_LOCATION}/testing_data/* output/$Model/testing_data
+    eos cp ${MODEL_LOCATION}/signal_process_data.tgz .
+    tar -xf signal_process_data.tgz
+    python tagger/train/train.py --plot-basic -sig $SIGNAL -y tagger/model/configs/$Model.yaml -o output/$Model
+
+elif [ "$1" == "True" ] && [ "$2" == "False" ]; then
+    mkdir -p output/$Model/model
+    eos cp ${MODEL_LOCATION}/model/saved_model.* output/$Model/model
     mkdir -p output/$Model/testing_data
     eos cp ${MODEL_LOCATION}/testing_data/* output/$Model/testing_data
     eos cp ${MODEL_LOCATION}/signal_process_data.tgz .

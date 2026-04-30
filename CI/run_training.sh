3566f00c739637be7f26de96b75ab73fcbe26dea
schaefes <86061473+schaefes@users.noreply.github.com>
2026-04-30 11:17:34 +0200

    Create plots directory for weightedAverageSimple
    Add directory for weighted average simple training plots

--- CI/run_training.sh
+++ CI/run_training.sh
@@ -24,6 +24,7 @@ elif [ "$1" == "False" ] && [ "$2" == "True" ]; then
     eos cp ${MODEL_LOCATION}/testing_data/* output/$Model/testing_data
     eos cp ${EOS_STORAGE_DIR}/${EOS_STORAGE_DATADIR}/signal_process_data.tgz .
     tar -xf signal_process_data.tgz
+    mkdir -p output/weightedAverageSimple/plots/training
     python tagger/train/train.py --plot-basic -sig $SIGNAL -y tagger/model/configs/$Model.yaml -o output/$Model
 
 elif [ "$1" == "True" ] && [ "$2" == "False" ]; then

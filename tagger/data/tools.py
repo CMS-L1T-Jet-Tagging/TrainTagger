2652a353e97aac7a5d63dd5c607110b9bda0179a
Stella <stella.felice.schaefer@desy.de>
2026-05-11 12:13:20 +0200

    forgot to add argument in method def

--- tagger/data/tools.py
+++ tagger/data/tools.py
@@ -31,7 +31,7 @@ def _add_response_vars(data):
         data['jet_pt_raw'] / data['jet_genmatch_pt'], copy=True, nan=0.0, posinf=0.0, neginf=0.0
     )
 
-def _split_flavor(data):
+def _split_flavor(data, use_pileup):
     """
     Splits data by particle flavor and applies conditions for each category. Also creates the pT target.
 

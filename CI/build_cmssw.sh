0841ef935ab300cfd2683a96303495a8be8123c6
schaefes <86061473+schaefes@users.noreply.github.com>
2026-05-13 12:13:48 +0200

    Update runPerformanceNtuple.py with model path
    Add environment variable for l1tSC4NGJetModelPath to runPerformanceNtuple.py

--- CI/build_cmssw.sh
+++ CI/build_cmssw.sh
@@ -70,6 +70,7 @@ fi;
 scram b 2>&1 || exit 1
 
 cd FastPUPPI/NtupleProducer/python
+echo $'\nprocess.l1tSC4NGJetProducer.l1tSC4NGJetModelPath = cms.string(os.environ["CMSSW_BASE"]+"/src/L1TSC4NGJetModel/L1TSC4NGJetModel_PtPU1/L1TSC4NGJetModel_PtPU1")' >> runPerformanceNtuple.py
 cmsenv
 ./scripts/prun.sh runPerformanceNTuple.py --151X_v1 ${PROC} '' --nomerge
 cd ${PROC}

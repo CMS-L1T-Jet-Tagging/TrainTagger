0e400c84fda9770df20b91fd0a29a74f14ee1327
Christopher Brown <chris.brown@fpsl.net>
2026-06-12 10:20:52 +0200

    Try different import for profiling

--- tagger/firmware/hls4ml_profile.py
+++ tagger/firmware/hls4ml_profile.py
@@ -13,6 +13,8 @@ from tagger.model.common import fromFolder
 from tagger.plot import style
 from tagger.plot.common import plot_2d
 
+from hls4ml.model import profiling
+
 style.set_style()
 
 
@@ -90,14 +92,15 @@ def doPlots(model, outputdir, inputdir):
     figure.savefig("%s/%s_score_2D.pdf" % (outputdir, "Regression"), bbox_inches='tight')
     plt.close()
 
-    wp, wph, ap, aph = hls4ml.model.profiling.numerical(model=model.jet_model, hls_model=model.hls_jet_model, X=X_test)
+    print(hls4ml.__version__)
+    wp, wph, ap, aph = profiling.numerical(model=model.jet_model, hls_model=model.hls_jet_model, X=X_test)
     ap.savefig(outputdir + "/model_activations_profile.png")
     wp.savefig(outputdir + "/model_weights_profile.png")
     aph.savefig(outputdir + "/model_activations_profile_opt.png")
     wph.savefig(outputdir + "/model_weights_profile_opt.png")
 
     y_hls, hls4ml_trace = model.hls_jet_model.trace(np.ascontiguousarray(X_test))
-    keras_trace = hls4ml.model.profiling.get_ymodel_keras(model.jet_model, X_test)
+    keras_trace = profiling.get_ymodel_keras(model.jet_model, X_test)
 
     for layer in hls4ml_trace.keys():
         print("Doing profiling 2d for layer", layer)

2c4edee538ef287a74551c64ee1d1c6d71f5a69f
Christopher Brown <chris.brown@fpsl.net>
2026-06-18 09:00:31 +0200

    Use el8 gcc 13

--- CI/setup_cmssw.sh
+++ CI/setup_cmssw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export SCRAM_ARCH=el9_amd64_gcc14
+export SCRAM_ARCH=el8_amd64_gcc13
 
 if [[ "$2" == "" ]]; then
     echo "Usage $0 [ -checkout | -compile | -run ] CMSSW_VERSION GITHUB_MASTER GITHUB_TAG [ GITHUB_PR ]"

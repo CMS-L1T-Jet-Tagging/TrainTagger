3d341ead110656498b9521ad6a8f32208f843949
Stella <stella.felice.schaefer@desy.de>
2026-06-24 09:40:07 +0200

    highmem machines

--- tagger/plot/basic.py
+++ tagger/plot/basic.py
@@ -182,7 +182,7 @@ def ROC_binary(y_pred, y_test, class_labels, plot_dir, class_pair, signal_proc=N
 def ROC(y_pred, y_test, class_labels, plot_dir, ROC_dict):
     # Create a colormap for unique colors
     # Use 'tab10' with enough colors
-    colormap = cm.get_cmap('Set1', len(class_labels))
+    colormap = matplotlib.colormaps.get_cmap('Set1')
 
     # Create a plot for ROC curves
     fig, ax = plt.subplots(1, 1, figsize=style.FIGURE_SIZE)
@@ -428,7 +428,7 @@ def get_response(x_variable, bins, truth_pt, reco_pt, pt_ratio, pt_mask, reduce)
 
 def hist_response(pt_ratio, y_test, class_labels, plot_dir):
     fig, ax = plt.subplots(1, 1, figsize=style.FIGURE_SIZE)
-    colormap = cm.get_cmap('Set1', len(class_labels))
+    colormap = matplotlib.colormaps.get_cmap('Set1')
     save_path = os.path.join(plot_dir, "hist_response")
     os.makedirs(save_path, exist_ok=True)
     for i, (k, v) in enumerate(class_labels.items()):

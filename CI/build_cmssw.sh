424c9c52e06fdf4f617b74ac66663f9843ef0bcb
Christopher Brown <chris.brown@fpsl.net>
2026-07-27 17:18:49 +0200

    Use emulator_test_new_cmssw cms-hls4ml branch

--- CI/build_cmssw.sh
+++ CI/build_cmssw.sh
@@ -48,7 +48,7 @@ git clone --quiet https://github.com/Xilinx/HLS_arbitrary_Precision_Types.git hl
 
 git clone --quiet ${CMSSW_EMULATOR_WRAPPER}
 cd L1TSC4NGJetModel
-git checkout model_wrapper_v2
+git checkout emulator_test_new_cmssw
 
 cp -r ../../../output/$Model/firmware/L1TSC4NGJetModel/firmware .
 ./setup.sh test v2_0_0

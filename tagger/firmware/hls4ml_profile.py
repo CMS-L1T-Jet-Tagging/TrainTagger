188a2aa7bba64a0a0ca0d6f13e3431717e38cf15
Stella <stella.felice.schaefer@desy.de>
2026-07-31 15:26:57 +0200

    multiinput in profiling

--- tagger/firmware/hls4ml_profile.py
+++ tagger/firmware/hls4ml_profile.py
@@ -108,7 +108,13 @@ def doPlots(model, outputdir, inputdir):
     plt.close()
 
     print(hls4ml.__version__)
-    wp, wph, ap, aph = profiling.numerical(model=model.jet_model, hls_model=model.hls_jet_model, X=X_test)
+    model_dict = {k: np.ascontiguousarray(v, dtype=np.float64) for k, v in model_dict.items()}
+    hls_inputs = []
+    for var in model.hls_jet_model.get_input_variables():
+        name = var.name
+        hls_inputs.append(np.ascontiguousarray(model_dict[name], dtype=np.float64))
+    hls_inputs = hls_inputs[0] if len(hls_inputs) == 1 else hls_inputs
+    wp, wph, ap, aph = profiling.numerical(model=model.jet_model, hls_model=model.hls_jet_model, X=hls_inputs)
     ap.savefig(outputdir + "/model_activations_profile.png")
     wp.savefig(outputdir + "/model_weights_profile.png")
     aph.savefig(outputdir + "/model_activations_profile_opt.png")

c2e5a9bccf2fdae424d5e131434c3bdcc5b3fd65
Christopher Brown <chris.brown@fpsl.net>
2026-08-07 16:05:00 +0200

    Change fast puppi to CMSSW_17_0_0_pre2_NGJet_plus_correlator

--- CI/setup_cmssw.sh
+++ CI/setup_cmssw.sh
@@ -46,7 +46,7 @@ make
 make install
 cd ..
 
-git clone https://github.com/CMS-L1T-Jet-Tagging/FastPUPPI.git -b 20_0_X_NGJet
+git clone https://github.com/CMS-L1T-Jet-Tagging/FastPUPPI.git -b CMSSW_17_0_0_pre2_NGJet_plus_correlator
 
 
 if [[ "$COMPILE" == "false" ]]; then exit 0; fi

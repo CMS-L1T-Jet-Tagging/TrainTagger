3e1f8b273f50b2cdd6e512852650a1be7e0a0533
Jeronimo Antonio Segal <jeronimo.antonio.segal@cern.ch>
2026-08-10 11:39:45 +0100

    model_updates

--- tagger/model/FloatingDeepSetModel.py
+++ tagger/model/FloatingDeepSetModel.py
@@ -361,148 +361,46 @@ class FloatingDeepSetEmbeddingModel(JetTagModel):
         }
 
         L, C = inputs_shape
-        #Initialize inputs
+
+        # Initialize inputs
         inputs = keras.layers.Input(shape=inputs_shape, name='model_input')
         
-        #Main branch
         main = BatchNormalization(name='norm_input')(inputs)
                         
-        # Make Conv1D layers
+        # Encoder
         for iconv1d, depthconv1d in enumerate(self.model_config['conv1d_layers']):
             main = Conv1D(filters=depthconv1d, kernel_size=1, name='Conv1D_' + str(iconv1d + 1), activation='relu',**self.common_args)(main)
-
-        # Linear activation to change HLS bitwidth to fix overflow in AveragePooling
-        #main = AveragePooling1D(L,name='avgpool')(main)
-        #main = Flatten()(main)
         
-        main = GlobalAveragePooling1D(data_format='channels_last',name="pool")(main)
+        main = GlobalAveragePooling1D(data_format='channels_last', name="pool")(main)
 
-        
-        self.backbone_model = keras.Model(inputs=inputs, outputs=main)
+        # Projection head
+        proj = BatchNormalization(name='norm_embedding')(main)
+        for iproj, depthproj in enumerate(self.model_config['projection_layers'][:-1]):
+            proj = Dense(depthproj, name='Dense_' + str(iproj + 1) + '_proj', activation='relu', **self.common_args)(proj)
+        proj = Dense(self.model_config['projection_layers'][-1], name='Dense_' + str(len(self.model_config['projection_layers'])) + '_proj', activation='linear', **self.common_args)(proj)
 
-        initializer = self.model_config['kernel_initializer']#keras.initializers.RandomNormal(mean=0.0, stddev=0.001)
-        #Now split into jet ID and pt regression
+        # Jet ID and pT regression heads
         bn_main = BatchNormalization(name='norm_embedding')(main)
-        # Make fully connected dense layers for classification task
-        for iclass, depthclass in enumerate(self.model_config['classification_layers']):
-            if iclass == 0:
-                jet_id = Dense(depthclass, name='Dense_' + str(iclass + 1) + '_jetID', kernel_initializer=initializer)(bn_main)
-            else:
-                jet_id = Dense(depthclass, name='Dense_' + str(iclass + 1) + '_jetID', activation='relu', kernel_initializer=initializer)(jet_id)
 
-        jet_id = Dense(outputs_shape[0], name='Dense_3_jetID',activation='linear',kernel_initializer=initializer)(jet_id)
+        for iclass, depthclass in enumerate(self.model_config['classification_layers']):
+            jet_id = Dense(depthclass, name='Dense_' + str(iclass + 1) + '_jetID', activation='relu', **self.common_args)(bn_main if iclass == 0 else jet_id)
+        jet_id = Dense(outputs_shape[0], name='Dense_3_jetID', activation='linear', **self.common_args)(jet_id)
         jet_id = Activation('softmax', name='jet_id_output')(jet_id)
 
-        #pT regression branch
-        
-        # Make fully connected dense layers for pt regression task
         for ireg, depthreg in enumerate(self.model_config['regression_layers']):
-            if ireg == 0:
-                pt_regress = Dense(depthreg, name='Dense_' + str(ireg + 1) + '_pT', kernel_initializer=initializer)(bn_main)
-            else:
-                pt_regress = QDense(depthreg, name='Dense_' + str(ireg + 1) + '_pT', activation='relu',kernel_initializer=initializer)(pt_regress)
+            pt_regress = Dense(depthreg, name='Dense_' + str(ireg + 1) + '_pT', activation='relu', **self.common_args)(bn_main if ireg == 0 else pt_regress)
+        pt_regress = Dense(1, name='pT_output', **self.common_args)(pt_regress)
 
-        pt_regress = Dense(1, name='pT_output',
-                            kernel_initializer=initializer)(pt_regress)
+        self.backbone_model = keras.Model(inputs=inputs, outputs=main)
+        self.embedding_model = keras.Model(inputs=inputs, outputs=proj)
+        self.jet_model = keras.Model(inputs=inputs, outputs=[jet_id, pt_regress])
 
-        #Define the model using both branches
-        self.create_encoder(inputs_shape,self.model_config['projection_dims'])
-                
-        self.jet_model = keras.Model(inputs, [jet_id,pt_regress])
-                
+        print("========== Embedding Model Summary ==========")
         print(self.embedding_model.summary())
-        
+
+        print("========== Jet Model Summary ==========")
         print(self.jet_model.summary())
         
-    def create_encoder(self,inputs_shape, projection_dim,projection_blocks=4):
-
-        inputs  = keras.Input(inputs_shape)
-        #inputs = tf.keras.Input(shape=(28, 28, 1))
-        features = self.backbone_model(inputs)
-
-        # Projection head, the z's remember?
-        outputs = keras.Sequential([
-            Dense(projection_dim,activation='linear'),
-            BatchNormalization(),
-            ReLU(),
-            Dense(projection_dim,activation='linear'),
-            BatchNormalization(),
-            ReLU(),
-            Dense(projection_dim,activation='linear'),
-            BatchNormalization(),
-            ReLU(),
-            Dense(projection_dim,activation='linear'),
-            BatchNormalization(),
-            ReLU(),
-        ])(features)
-        # Normalize to unit vectors so dot product equals cosine similarity (required for contrastive loss)
-        outputs = Dense(projection_dim, use_bias=False,activation='linear')(outputs)
-        outputs = L2NormalizeLayer()(outputs)
-        self.embedding_model = keras.Model(inputs, outputs)
-
-    def firmware_convert(self, firmware_dir: str, build: bool = False):
-        """Run the hls4ml model conversion
-
-        Args:
-            firmware_dir (str): Where to save the firmware
-            build (bool, optional): Run the full hls4ml build? Or just create the project. Defaults to False.
-        """
-
-        # Remove the old directory if it exists
-        hls4ml_outdir = firmware_dir + '/' + self.firmware_config['project_name']
-        os.system(f'rm -rf {hls4ml_outdir}')
-
-        # Create default config
-        config = hls4ml.utils.config_from_keras_model(self.jet_model, granularity='name')
-        config['IOType'] = 'io_parallel'
-        config['LayerName']['model_input']['Precision']['result'] = self.firmware_config['input_precision']
-
-        # Configuration for conv1d layers
-        # hls4ml automatically figures out the paralellization factor
-        # config['LayerName']['Conv1D_1']['ParallelizationFactor'] = 8
-        # config['LayerName']['Conv1D_2']['ParallelizationFactor'] = 8
-
-        # Additional config
-        for layer in self.jet_model.layers:
-            layer_name = layer.__class__.__name__
-
-            if layer_name in ["BatchNormalization", "InputLayer"]:
-                config["LayerName"][layer.name]["Precision"] = self.firmware_config['input_precision']
-                config["LayerName"][layer.name]["result"] = self.firmware_config['input_precision']
-                config["LayerName"][layer.name]["Trace"] = not build
-
-            elif layer_name in ["Permute", "Concatenate", "Flatten", "Reshape", "UpSampling1D", "Add"]:
-                print("Skipping trace for:", layer.name)
-            else:
-                config["LayerName"][layer.name]["Trace"] = not build
-
-        config["LayerName"]["jet_id_output"]["Precision"]["result"] = self.firmware_config['class_precision']
-        config["LayerName"]["jet_id_output"]["Implementation"] = "latency"
-        config["LayerName"]["pT_output"]["Precision"]["result"] = self.firmware_config['reg_precision']
-        config["LayerName"]["pT_output"]["Implementation"] = "latency"
-
-        # Write HLS
-        self.hls_jet_model = hls4ml.converters.convert_from_keras_model(
-            self.jet_model,
-            backend='Vitis',
-            project_name=self.firmware_config['project_name'],
-            clock_period=self.firmware_config['clock_period'],
-            hls_config=config,
-            output_dir=f'{hls4ml_outdir}',
-            part= self.firmware_config['fpga_part'],
-        )
-
-        # Compile the project
-        self.hls_jet_model.compile()
-
-        # Save config  as json file
-        print("Saving default config as config.json ...")
-        with open(hls4ml_outdir + '/config.json', 'w') as fp:
-            json.dump(config, fp)
-
-        if build:
-            # build the project
-            self.hls_jet_model.build(csim=False, reset=True)
 
     def compile_model(self, num_samples: int):
         """compile the model generating callbacks and loss function
@@ -510,19 +408,18 @@ class FloatingDeepSetEmbeddingModel(JetTagModel):
             num_samples (int): Number of samples in the training set used for scheduling
         """
 
-        # Define the callbacks using hyperparameters in the config
-
+        # Pretraining
         steps = self.training_config['embedding_epochs'] * (num_samples // self.training_config['batch_size'])
         scheduler = keras.optimizers.schedules.CosineDecay(
             initial_learning_rate=self.training_config['embedding_lr'], decay_steps=steps
         )
 
-        # Create an early stopping callback.
-        early_stopping = keras.callbacks.EarlyStopping(
-            monitor="loss", patience=5, restore_best_weights=True
-        )
-        
-        self.embedding_callbacks = [early_stopping]
+        self.embedding_callbacks = [
+            keras.callbacks.EarlyStopping(
+                monitor="loss", patience=5, restore_best_weights=True
+            )
+        ]
+        self.embedding_optimizer = tf.keras.optimizers.Adam(scheduler)
         
         self.fine_tune_callbacks = [
             EarlyStopping(monitor='val_loss', patience=self.training_config['EarlyStopping_patience']),
@@ -535,7 +432,6 @@ class FloatingDeepSetEmbeddingModel(JetTagModel):
         ]
         
         self.fine_tuning_optimizer = tf.keras.optimizers.Adam(learning_rate=self.training_config['learning_rate'])
-        self.constrastive_optimizer = tf.keras.optimizers.Adam(scheduler)
 
         # compile the tensorflow model setting the loss and metrics
         self.jet_model.compile(
@@ -583,17 +479,14 @@ class FloatingDeepSetEmbeddingModel(JetTagModel):
         augment = SimCLRPreprocessing(0.2)
         train_ds = (
             tf.data.Dataset.from_tensor_slices((x_train,y_train,sample_weight))
-            .shuffle(self.training_config['batch_size'])
+            .shuffle(20_000, reshuffle_each_iteration=True)
             .map(augment, num_parallel_calls=tf.data.AUTOTUNE)
             .batch(self.training_config['batch_size'])
             .prefetch(tf.data.AUTOTUNE)
         )
 
-        optimizer = self.constrastive_optimizer
-        embedding_model = self.embedding_model
-        self.embedding_model.optimizer = optimizer
         self.embedding_model.loss = SimCLRLoss()
-        callbacks = keras.callbacks.CallbackList(self.embedding_callbacks, add_history=True, model=embedding_model)
+        callbacks = keras.callbacks.CallbackList(self.embedding_callbacks, add_history=True, model=self.embedding_model)
         logs = {}
         callbacks.on_train_begin(logs=logs)
 
@@ -610,7 +503,7 @@ class FloatingDeepSetEmbeddingModel(JetTagModel):
             # Compute gradients of loss w.r.t. model trainable weights
             grads = tape.gradient(loss, self.embedding_model.trainable_weights)
             # Apply gradients to update model weights using optimizer
-            optimizer.apply_gradients(zip(grads, self.embedding_model.trainable_weights))
+            self.embedding_optimizer.apply_gradients(zip(grads, self.embedding_model.trainable_weights))
             # Return the computed loss for logging
             return loss
 
